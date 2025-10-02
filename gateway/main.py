c5caa7a39f607dcb25e31dde65d0f36e742468b6
Manuel Quintana <mquintanau@unal.edu.co>
2025-10-02 05:52:24 -0500

    add all allowed origins

--- gateway/main.py
+++ gateway/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from ariadne.asgi import GraphQL
 from .resolvers import schema
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Define allowed origins for CORS
+origins = [
+    "*"
+]
+
+# Add CORS middleware to the application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Crear la aplicación ASGI de GraphQL
 graphql_app = GraphQL(schema, debug=True)
